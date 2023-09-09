5b89fe4a7fed39ec224715078f2cf521331738f5
Maciej Wiącek <maciejwiacek4@gmail.com>
2023-09-09 10:30:42 +0200

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Maciej Wiącek on 09/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
